01343226293ce1ac336264093aea71ac678518a3
Jerry-Guo1020 <2593517388@qq.com>
2025-07-02 12:18:43 +0800

    更新了登录的逻辑

--- dao/loginDao.js
+++ dao/loginDao.js
@@ -1,5 +1,61 @@
 const mysql = require('../db/mysql57');
 
+// {{ edit_1 }}
+// 根据昵称查询用户
+exports.getUserByNickname = async (nickname) => {
+  try {
+    console.log('【loginDao】查询 nickname:', nickname);
+    const sql = 'SELECT * FROM User WHERE nickname = ? LIMIT 1';
+    const users = await mysql.sqlExec(sql, [nickname]);
+    console.log('【loginDao】查询 nickname 结果:', users);
+    return users[0];
+  } catch (err) {
+    console.error('loginDao.getUserByNickname 错误:', err);
+    throw err;
+  }
+};
+
+// 根据用户ID查询用户
+exports.getUserById = async (id) => {
+  try {
+    console.log('【loginDao】查询用户ID:', id);
+    const sql = 'SELECT * FROM User WHERE id = ? LIMIT 1';
+    const users = await mysql.sqlExec(sql, [id]);
+    console.log('【loginDao】查询用户ID 结果:', users);
+    return users[0];
+  } catch (err) {
+    console.error('loginDao.getUserById 错误:', err);
+    throw err;
+  }
+};
+
+// 添加新用户 (适配游客登录字段)
+exports.addUser = async (user) => {
+  try {
+    console.log('【loginDao】准备注册新用户:', user);
+    // 注意：这里根据你的 User 表结构，可能需要调整插入的字段
+    // 确保 User 表有 nickname, avatar, username, sex, isNewUser 等字段
+    const sql = `
+      INSERT INTO User (userTypeId, nickname, avatar, username, sex, isNewUser)
+      VALUES (?, ?, ?, ?, ?, ?)`;
+    const data = [
+      user.userTypeId,
+      user.nickname,
+      user.avatar,
+      user.username,
+      user.sex,
+      user.isNewUser
+    ];
+    const result = await mysql.sqlExec(sql, data);
+    console.log('【loginDao】注册新用户结果:', result);
+    return result.insertId; // 返回新插入的ID
+  } catch (err) {
+    console.error('loginDao.addUser 错误:', err);
+    throw err;
+  }
+};
+
+// 原有的 getUserByOpenid 如果不再使用，可以删除或注释掉
 exports.getUserByOpenid = async (openid) => {
   try {
     console.log('准备查用户', openid);
@@ -9,22 +65,6 @@ exports.getUserByOpenid = async (openid) => {
     return users[0];
   } catch (err) {
     console.error('loginDao.getUserByOpenid 错误:', err);
-    throw err;
-  }
-};
-
-exports.addUser = async (user) => {
-  try {
-    console.log('准备注册新用户', user);
-    const sql = `
-      INSERT INTO User (userTypeId, openid, nickname, avatar, sex)
-      VALUES (?, ?, ?, ?, ?)`;
-    const data = [user.userTypeId, user.openid, user.nickname, user.avatar, user.sex];
-    const result = await mysql.sqlExec(sql, data);
-    console.log('注册新用户结果', result);
-    return result.insertId || (result[0] && result[0].insertId);
-  } catch (err) {
-    console.error('loginDao.addUser 错误:', err);
     throw err;
   }
 };
--- services/loginService.js
+++ services/loginService.js
@@ -1,30 +1,42 @@
 const loginDao = require('../dao/loginDao');
-const { getOpenidByCode } = require('../utils/wechat');
+// const { getOpenidByCode } = require('../utils/wechat'); // 如果不再使用微信登录，可以移除此行
 const { signJwtToken } = require('../utils/jwt');
 
-exports.wechatLogin = async (wxLogin) => {
-  try {
-    const { code, userInfo } = wxLogin;
-    const openid = await getOpenidByCode(code);
-    if (!openid) { // 只有当 openid 为 null 或 undefined 时，才会进入这里
-      return { code: 400, msg: "获取openid失败", data: null };
-    }
+// {{ edit_1 }}
+// 游客登录逻辑
+exports.guestLogin = async (userData) => {
+  console.log('【loginService】进入 guestLogin 方法');
+  console.log('接收到的用户数据:', userData);
 
-    let user = await loginDao.getUserByOpenid(openid);
+  try {
+    const { nickname, avatar } = userData;
+
+    // 1. 根据昵称查询用户
+    let user = await loginDao.getUserByNickname(nickname);
+    console.log('【loginService】查询用户结果:', user ? '已存在' : '不存在');
 
     if (!user) {
+      // 2. 如果用户不存在，则创建新用户
       const newUser = {
-        openid,
-        nickname: userInfo.nickName,
-        avatar: userInfo.avatarUrl,
-        sex: userInfo.gender || null,
+        nickname,
+        avatar,
         userTypeId: 2, // 普通用户
+        username: nickname, // 假设 username 默认就是 nickname
+        sex: '男', // 默认值，如果前端没有提供
+        isNewUser: 1 // 标记为新用户
       };
-      await loginDao.addUser(newUser);
-      user = await loginDao.getUserByOpenid(openid);
+      console.log('【loginService】准备创建新用户:', newUser);
+      const newUserId = await loginDao.addUser(newUser);
+      user = await loginDao.getUserById(newUserId); // 重新查询新创建的用户信息
+      console.log('【loginService】新用户创建成功，ID:', newUserId);
+    } else {
+      console.log('【loginService】用户已存在，直接登录');
+      // 如果用户已存在，你可能需要更新一些信息，或者直接返回
     }
 
-    const token = signJwtToken({ uid: user.id, openid: user.openid });
+    // 3. 生成 JWT token
+    const token = signJwtToken({ uid: user.id, nickname: user.nickname });
+    console.log('【loginService】生成 token 成功');
 
     return {
       code: 200,
@@ -33,7 +45,6 @@ exports.wechatLogin = async (wxLogin) => {
         token,
         user: {
           id: user.id,
-          openid: user.openid,
           nickname: user.nickname,
           avatar: user.avatar,
           sex: user.sex,
@@ -45,6 +56,42 @@ exports.wechatLogin = async (wxLogin) => {
           createTime: user.createTime
         }
       }
+    };
+  } catch (err) {
+    console.error('【loginService】guestLogin 错误:', err);
+    return { code: 500, msg: "登录失败", data: null };
+  }
+};
+
+// 原有的 wechatLogin 如果不再使用，可以删除或注释掉
+exports.wechatLogin = async (wxLogin) => {
+  try {
+    const { code, userInfo } = wxLogin;
+    // const openid = await getOpenidByCode(code); // 如果移除了 getOpenidByCode，这里会报错
+    // if (!openid) {
+    //   return { code: 400, msg: "获取openid失败", data: null };
+    // }
+
+    // let user = await loginDao.getUserByOpenid(openid);
+
+    // if (!user) {
+    //   const newUser = {
+    //     openid,
+    //     nickname: userInfo.nickName,
+    //     avatar: userInfo.avatarUrl,
+    //     sex: userInfo.gender || null,
+    //     userTypeId: 2, // 普通用户
+    //   };
+    //   await loginDao.addUser(newUser);
+    //   user = await loginDao.getUserByOpenid(openid);
+    // }
+
+    // const token = signJwtToken({ uid: user.id, openid: user.openid });
+
+    return {
+      code: 501, // 假设不再支持微信登录，返回一个未实现的状态码
+      msg: "微信登录功能已禁用",
+      data: null
     };
   } catch (err) {
     console.error('loginService.wechatLogin错误:', err);
